4d410a9cf63d88536f401d99ee520e73be618c7c
FaabScheeren <f.scheeren@outlook.com>
2020-09-14 13:55:30 +0200

    Horizon pod install

---
+++ mindset_app_frontend/Controller/CalendarViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CalendarViewController.swift
+//  mindset_app_frontend
+//
+//  Created by Fabio Scheeren on 14/09/2020.
+//  Copyright © 2020 Fabio Scheeren. All rights reserved.
+//
+
+import UIKit
+
+
+class CalendarViewController: UIViewController {
+//    let calendarView = CalendarView(initialContent: makeContent())
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
